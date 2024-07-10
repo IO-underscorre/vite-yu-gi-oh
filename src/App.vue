957e55477c7fbb9815db1e7d848564ad3f2a4740
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-07-10 07:07:15 +0200

    feat(CardsSearcher): card searcher macro component completed

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
+import CardsSearcher from './components/CardsSearcher.vue';
 
 export default {
   name: 'App',
 
   components: {
     AppHeader,
+    CardsSearcher
   }
 }
 </script>
 
 <template>
   <AppHeader />
+
+  <main>
+    <CardsSearcher />
+  </main>
 </template>
 
 <style lang="scss">
 @use './styles/general.scss' as *;
+
+main {
+  max-width: 105rem;
+  width: 95%;
+  margin: 0 auto;
+}
 </style>
---
+++ src/components/CardsSearcher.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+    name: 'CardsSearcher',
+
+}
+</script>
+
+<template>
+    <div class="cards-founded-container">
+        <div class="cards-list-settings">
+            <b>
+                Found placeholder cards
+            </b>
+
+            <div>
+                <label for="cards-showed">Cards showed for page: </label>
+                <select id="cards-showed">
+                    <option value="10" selected>10</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                </select>
+            </div>
+        </div>
+
+        <ul class="cards-list">
+            <li>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.cards-founded-container {
+    padding: 4.5rem;
+    background-color: $clr-neutral-ltr;
+
+    .cards-list-settings {
+        background-color: $clr-neutral-dkr;
+        color: $clr-neutral-ltr;
+        padding: 1rem;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .cards-list {
+        margin-top: 1rem;
+        display: flex;
+        gap: 1rem;
+
+        &>li {
+            flex-basis: calc((100% - 4rem) / 5);
+        }
+    }
+}
+</style>
