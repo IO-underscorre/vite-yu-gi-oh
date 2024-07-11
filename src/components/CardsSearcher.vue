3b8b09c016d7c9319de10ae021a0ca70e908a8b2
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-07-11 06:40:39 +0200

    feat(CardsSearcher.vue): added a paging system to display only the selected number of cards per page

--- src/components/CardsSearcher.vue
+++ src/components/CardsSearcher.vue
@@ -15,7 +15,10 @@ export default {
 
     data() {
         return {
-            store
+            store,
+
+            numberOfCardsPerPageString: '5',
+            pageNumber: 0
         }
     },
 
@@ -27,6 +30,7 @@ export default {
                 let completeApiURL = store.apiURL + 'cardinfo.php?archetype=' + archetypeFilterString
                 axios.get(completeApiURL).then(callReturn => {
                     store.cardsSearchResult = callReturn.data.data;
+                    this.pageNumber = 0;
                 }).catch(error => {
                     store.cardSearchError = true;
                 });
@@ -37,6 +41,29 @@ export default {
     computed: {
         shouldFiltersBeDisabled() {
             return store.searchParametersCallError || !store.isApiBeenCalledForFilters ? true : false;
+        },
+
+        getNumberOfCardsPerPage() {
+            return parseInt(this.numberOfCardsPerPageString);
+        },
+
+        getFirstCardInNextPageIndex() {
+            return (this.pageNumber + 1) * this.getNumberOfCardsPerPage;
+        },
+
+        getCurrentShownCards() {
+            let firstCardInCurrentPageIndex = this.pageNumber * this.getNumberOfCardsPerPage;
+            let firstCardInNextPageIndex = this.getFirstCardInNextPageIndex < store.cardsSearchResult.length ? this.getFirstCardInNextPageIndex : undefined;
+
+            return store.cardsSearchResult.slice(firstCardInCurrentPageIndex, firstCardInNextPageIndex);
+        },
+
+        shouldPreviousPageButtonBeDisabled() {
+            return this.pageNumber < 1 ? true : false;
+        },
+
+        shouldNextPageButtonBeDisabled() {
+            return this.getFirstCardInNextPageIndex >= store.cardsSearchResult.length ? true : false;
         }
     }
 }
@@ -53,21 +80,35 @@ export default {
             </b>
 
             <div>
-                <label for="cards-showed">Cards showed for page: </label>
-                <select id="cards-showed">
-                    <option value="10" selected>10</option>
+                <label for="cards-showed">Cards per page: </label>
+                <select id="cards-showed" v-model="numberOfCardsPerPageString" @change="pageNumber = 0">
+                    <option value="5" selected>5</option>
+                    <option value="10">10</option>
                     <option value="20">20</option>
-                    <option value="50">50</option>
                 </select>
             </div>
         </div>
 
         <template v-if="!cardSearchError && !store.searchParametersCallError">
             <ul class="cards-list">
-                <li v-for="card in store.cardsSearchResult" :key="card.id">
+                <li v-for="card in getCurrentShownCards" :key="card.id">
                     <CardContainer :cardName="card.name" :cardArchetype="card.archetype" cardImg="" />
                 </li>
             </ul>
+
+            <menu class="card-list-options">
+                <li>
+                    <button :class="{ disabled: shouldPreviousPageButtonBeDisabled }"
+                        @click="pageNumber--">&#x2770;</button>
+                </li>
+                <li>
+                    {{ pageNumber + 1 }}
+                </li>
+                <li>
+                    <button :class="{ disabled: shouldNextPageButtonBeDisabled }"
+                        @click="pageNumber++">&#x2771;</button>
+                </li>
+            </menu>
         </template>
         <template v-else>
             <strong class="error-message">
@@ -92,13 +133,52 @@ export default {
     }
 
     .cards-list {
-        margin-top: 1rem;
+        margin: 1rem 0;
         display: flex;
+        flex-wrap: wrap;
         gap: 1rem;
 
         &>li {
             flex-basis: calc((100% - 4rem) / 5);
         }
+    }
+
+    .card-list-options {
+        text-align: center;
+
+        li {
+            display: inline-block;
+
+            button {
+                font-size: 1.5rem;
+                line-height: 1;
+                width: 2rem;
+                height: 2rem;
+                margin: 0 1rem;
+                appearance: none;
+                border-radius: .25rem;
+                border: 1px solid $clr-neutral-dkr;
+                background-color: $clr-neutral-dkr;
+                color: $clr-neutral-ltr;
+
+                &:hover {
+                    box-shadow: 0 0 .25rem #00000080;
+                    cursor: pointer;
+                }
+
+                &:active {
+                    box-shadow: inset 0 0 .5rem #00000080;
+                    filter: brightness(75%);
+                }
+            }
+        }
+    }
+
+    .error-message {
+        display: block;
+        font-size: 5vw;
+        padding: 5rem 0;
+        text-align: center;
     }
 }
 </style>
